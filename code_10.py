852d31d4418634c3ac1a9df86f31f4f80f2cfbf6
nernandez <70415076+nernandez@users.noreply.github.com>
2023-04-10 21:47:41 -0600

    Update code_10.py

--- code_10.py
+++ code_10.py
@@ -1,1 +1,13 @@
+import nltk
 
+reviews = open("tacos_reviews.txt")
+
+for review in reviews:
+    tokens = nltk.word_tokenize(review)
+    print(tokens)
+
+    pos_tags = nltk.pos_tag(tokens)
+    
+    for token in pos_tags:
+        if token[1] == 'JJ' or token[1] == 'NN':
+            print(token)
