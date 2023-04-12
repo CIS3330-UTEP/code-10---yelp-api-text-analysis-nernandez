7418337625f6e97174f08277964e2e4484f37cbf
nernandez <70415076+nernandez@users.noreply.github.com>
2023-04-12 11:44:30 -0600

    Update code_10.py

--- code_10.py
+++ code_10.py
@@ -1,13 +1,23 @@
 import nltk
+from nltk.corpus import stopwords
 
 reviews = open("tacos_reviews.txt")
+stop_words = set(stopwords.words('english'))
 
 for review in reviews:
     tokens = nltk.word_tokenize(review)
-    print(tokens)
-
     pos_tags = nltk.pos_tag(tokens)
-    
+    new_text = []
+    for tag in pos_tags:
+        if tag[0] not in stop_words:
+            print(tag[0])
+            new_text.append(tag[0])
+
     for token in pos_tags:
-        if token[1] == 'JJ' or token[1] == 'NN':
-            print(token)
+        if token[1] == 'JJ' or token[1] == 'JJS':
+            print(token[0])
+
+print("\nOriginal")
+print(review)
+print("\nNew")
+print("".join(new_text))
